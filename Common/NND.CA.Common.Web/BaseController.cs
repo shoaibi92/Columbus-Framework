c9f0a5da605e78658f2d0e7f340bb76117db62ac
K.D.Rad <davoudi.rk@gmail.com>
2016-11-10 11:16:20 -0800

    Adding BaseController.cs to the NND.CA.Common.Web.Common namespace

---
+++ Common/NND.CA.Common.Web/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace NND.CA.Common.Web
+{
+    /// <summary>
+    /// Base Controller
+    /// </summary>
+    public abstract class BaseController<T> : Controller
+    {
+    }
+}
