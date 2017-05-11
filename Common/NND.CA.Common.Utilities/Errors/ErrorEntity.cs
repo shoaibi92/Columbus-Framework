2ed3ab0d44d1081835254e8b5d6f013248672bd7
Kian <davoudi.rk@gmail.com>
2017-05-10 17:36:09 -0700

    Adding error entity to common project

---
+++ Common/NND.CA.Common.Utilities/Errors/ErrorEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NND.CA.Common.Utilities.Errors
+{
+    public class ErrorEntity
+    {
+
+        #region Cunstroctor
+        public ErrorEntity()
+        {
+
+        }
+        #endregion
+    }
+}
