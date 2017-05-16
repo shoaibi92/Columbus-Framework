69d3781a9a47c2ed1c960746c97d3a4e161fe23f
Kian <davoudi.rk@gmail.com>
2017-05-16 11:58:00 -0700

    Connection to Db Shrek successful

---
+++ Common/NND.CA.Common.Web/DBAccess/ShrekDbContext.cs
@@ -0,0 +1,18 @@
+﻿using NND.CA.Common.Web.ServiceReferences;
+using NND.WebApi.Models.Resources;
+using System.Data.Entity;
+
+namespace NND.CA.Common.Web.DBAccess
+{
+    public class ShrekDbContext : BaseDbContext
+    {
+        public const string DbContextConnectionString = "name=ShrekDbContextString";
+
+        public ShrekDbContext() : base(DbContextConnectionString)
+        {
+           // Database.SetInitializer<ShrekDbContext>(null);
+        }
+
+        public DbSet<UnbonceWebApiDataModel> UnbonceWebApiDataModels { get; set; }
+    }
+}
--- Common/NND.CA.Common.Web/DBAccess/TestDbContext.cs
+++ Common/NND.CA.Common.Web/DBAccess/TestDbContext.cs
@@ -7,7 +7,7 @@ using NND.CA.Common.Web.ServiceReferences;
 using System.Data.Entity;
 using NND.CA.Common.Model;
 
-// TODO: remove the mahic letter for connection string: 
+// TODO: remove the magic letter for connection string: 
 
 namespace NND.CA.Common.Web.DBAccess
 {
