799a64d959a5b76becc00317de6ffc98f7d42e1d
Kian <davoudi.rk@gmail.com>
2017-07-11 15:00:19 -0700

    adding new files for call tracking matrix

---
+++ Common/NND.CA.Common.Model/BaseModels/NND.CA.WebApi/IncomingDataModelsFromThirdPartyBaseModel.cs
@@ -0,0 +1,21 @@
+﻿/* Author: Kian D.Rad
+ * Date:   July 10th 2017
+ * README: In the WebApi, there are numbers of incoming data models. There is little contol over the 
+ * datamodels that are http posted by various vendors. In future, if I want to add some new attribute 
+ * I would like to do that homogeniously in here. So, all of those datamodels obey this class. 
+ * ToDo:   TBD. 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NND.CA.Common.Model.BaseModels.NND.CA.WebApi
+{
+    public class IncomingDataModelsFromThirdPartyBaseModel
+    {
+    }
+}
