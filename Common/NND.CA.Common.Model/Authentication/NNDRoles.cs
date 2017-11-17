63cb38ff20eafec1116579c796d2a29c0ef5b8a9
kian davoudi rad <davoudi.rk@gmail.com>
2017-11-17 14:59:03 -0800

    Adding new userIfo Data

---
+++ Common/NND.CA.Common.Model/Authentication/NNDRoles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NND.CA.Common.Model.Authentication
+{
+    public enum NNDRoles
+    {
+        /// <summary>
+        /// None
+        /// </summary>
+        //[EnumValue("SecurityType_None", typeof(EnumerationsResx))]
+        [StringValue("Admin")]
+        Admin = 1,
+
+        /// <summary>
+        /// Add
+        /// </summary>
+        //[EnumValue("SecurityType_Add", typeof(EnumerationsResx))]
+        [StringValue("FranchisePartner")]
+        FranchisePartner = 2,
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        //[EnumValue("SecurityType_Delete", typeof(EnumerationsResx))]
+        [StringValue("CareGiver")]
+        CareGiver = 3
+
+
+    }
+}
--- Common/NND.CA.Common.Model/Authentication/NNDUser.cs
+++ Common/NND.CA.Common.Model/Authentication/NNDUser.cs
@@ -2,16 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public partial class NNDUser
     {
+        [DataType(DataType.Custom)]
         public int ID { get; set; }
-        public string Role { get; set; }
+        public NNDRoles Role { get; set; }
+        [DataType(DataType.Text)]
         public string UserName { get; set; }
         public string LastName { get; set; }
+        [DataType(DataType.Text)]
         public string FirstName { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public Nullable<bool> isValid { get; set; }
+
     }
 }
